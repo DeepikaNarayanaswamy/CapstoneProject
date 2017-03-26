378b0a7658ed7083dbe3ed42920c4c2b88537379
DeepikaNarayanaswamy <dn79@gatech.edu>
2017-03-26 11:10:04 +0530

    Added sharing and admob features

--- NewsNow/app/src/main/java/com/deepika/newsnow/NewsDetail.java
+++ NewsNow/app/src/main/java/com/deepika/newsnow/NewsDetail.java
@@ -1,11 +1,19 @@
 package com.deepika.newsnow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class NewsDetail extends AppCompatActivity {
 
@@ -24,6 +32,38 @@ public class NewsDetail extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        MobileAds.initialize(getApplicationContext(), getString(R.string.banner_ad_unit_id));
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_newsdetail_tab, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_share:
+
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                TextView newsTitle = (TextView)findViewById(R.id.newsTITLE);
+                String shareBodyText = newsTitle.getText().toString();
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,"Subject here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBodyText);
+                startActivity(Intent.createChooser(sharingIntent, getString(R.string.sharing_option)));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
